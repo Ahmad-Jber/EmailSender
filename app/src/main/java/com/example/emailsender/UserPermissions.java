1afce54f74c12e41ef2d795daff9cbc6eef0a48e
AhmadJber <ahmad.jber2@students.alquds.edu>
2022-09-03 22:56:28 -0700

    INIT1

--- app/src/main/java/com/example/emailsender/UserPermissions.java
+++ app/src/main/java/com/example/emailsender/UserPermissions.java
@@ -16,19 +16,16 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class UserPermissions {
+
     public static final int PERMISSION_ID=200;
-    private static final String [] PERMISSIONS= new String[]{WRITE_VOICEMAIL, ACCESS_NETWORK_STATE, ACCESS_WIFI_STATE, INTERNET};
+
+    private static final String [] PERMISSIONS= new String[]{WRITE_VOICEMAIL};
+
     public static boolean checkPermissions(Context context){
-        return ContextCompat.checkSelfPermission(context, PERMISSIONS[0]) == PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(context, PERMISSIONS[1]) == PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(context, PERMISSIONS[2]) == PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(context, PERMISSIONS[3]) == PERMISSION_GRANTED;
-    }
-    public static void requestPermissions(Activity activity){
-        ActivityCompat.requestPermissions(activity,PERMISSIONS,PERMISSION_ID);
+        return ContextCompat.checkSelfPermission(context, PERMISSIONS[0]) == PERMISSION_GRANTED;
     }
 
-    public static String[] getPERMISSIONS() {
-        return PERMISSIONS;
-    }
-    public static boolean hasAllPermissionsGranted(@NonNull int[] grantResults) {
-        return grantResults[0] == PERMISSION_GRANTED && grantResults[1] == PERMISSION_GRANTED && grantResults[2] == PERMISSION_GRANTED && grantResults[3] == PERMISSION_GRANTED;
+    public static void requestPermissions(Activity activity){
+        ActivityCompat.requestPermissions(activity,PERMISSIONS,PERMISSION_ID);
     }
 }
